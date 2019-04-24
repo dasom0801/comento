a2e70522efe362599a84e76304fd8557df7a78f9
dasom <dev.dasom1012@gmail.com>
2019-04-25 02:56:20 +0900

    추가: 디테일 페이지 출력

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,14 +1,46 @@
 <template>
   <div class="detail">
-    detail {{ $route.params.id }}
+    <div class="article">
+      <h2 class="title">{{title}}</h2>
+      <p>{{email}}</p>
+      <p>{{date}}</p>
+      <p>{{contents}}</p>
+    </div>
+    <ul class="replies" v-if="replies.length">
+      <li v-for="reply in replies" :key="reply.no">
+        <p>{{reply.email}}</p>
+        <p>{{reply.updated_at}}</p>
+        <p>{{reply.contents}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Detail',
+  created () {
+    axios.get(`http://comento.cafe24.com/detail.php?req_no=${this.$route.params.id}`)
+      .then(function ({data}) {
+        const { article, replies } = data.detail
+        this.title = article.title
+        this.email = article.email
+        this.date = article.updated_at
+        this.contents = article.contents
+        this.replies = replies
+      }.bind(this))
+      .catch(error => {
+        console.log(error)
+      })
+  },
   data () {
     return {
+      title: '',
+      email: '',
+      contents: '',
+      date: '',
+      replies: []
     }
   }
 }
